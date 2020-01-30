6c47116346b75671e714d1d5581ac3faee71158f
Winston  Chung <18.chung.winston@ctkprep.org>
2020-01-30 01:56:33 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,7 +13,7 @@ class StudentsController < ApplicationController
   end
   
   def create
-    Student.create(params[:first_name], params[:last_name])
+    Student.create(params[:student][:first_name], params[:student][:last_name])
     redirect_to students_path
   end
 
